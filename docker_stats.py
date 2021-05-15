2662d58f4129d78a1b684583fba896d212b5959e
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2021-05-15 11:51:55 -0400

    Fixed: Bug in getting streamed data from docker.
    * Changed buffer length, and increased iterations to get more data from
    the socket.
    * JSONDecodeError has been solved and fixed.

--- docker_stats.py
+++ docker_stats.py
@@ -1,44 +1,89 @@
 import socket
 import json
 import time
+import os
+import csv
+
+SOCKET_PATH = '/var/run/docker.sock'
+HEADERS = ['timestamp', 'mem_percentage', 'cpu_percentage', 'num_pid']
+
 
 class DockerStats(object):
 
-    def __init__(self, container_name):
+    def __init__(self, container_name, file_path):
         self.container_name = container_name
-        self.raw_stats = None
-        self.cpu_usage = None
-        self.pid = None
-        self.memory_usage = None
-        self.collected_at = None
-        self.file_name = None
+        self.file_path = file_path
 
-    def collect_stats(self):
-        socket_path = '/var/run/docker.sock'
-        socket_connection = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        socket_connection.connect(socket_path)
-
+    @property
+    def connection_string(self):
         connection_string = f"GET http://localhost/containers/{self.container_name}/stats?stream=False HTTP/1.1\r\nHost: *\r\n\r\n".encode()
+        return connection_string
 
-        while True:
+    @property
+    def file_descriptor(self):
 
-            socket_connection.send(connection_string)
+        if os.path.exists(self.file_path):
+            
+            file_fd = open(self.file_path, 'a', newline='', encoding='utf-8')
+            csv_writer = csv.writer(file_fd)
+            return csv_writer
 
-            response_data = socket_connection.recv(10000)
-            content_lines = response_data.decode().split('\r\n')
-            print(content_lines[10])
-            version_dict = json.loads(content_lines[10])
-            out_files = open('example.json', 'w')
-            json.dump(version_dict, out_files)
-            # print(response_data)
-            time.sleep(1)
-        # version_dict = json.loads(content_lines[])
-        # print(version_dict)
+        else:
+            
+            file_fd = open(self.file_path, 'w', newline='', encoding='utf-8')
+            csv_writer = csv.writer(file_fd)
+            csv_writer.writerow(HEADERS)
+            return csv_writer
+
+
+def collect_stats(cont_stats: DockerStats):
+    socket_connection = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    socket_connection.connect(SOCKET_PATH)
+
+    conn_string = cont_stats.connection_string
+    csv_writer = cont_stats.file_descriptor
+    
+    while True:
+
+        socket_connection.send(conn_string)
+        complete_string = b""
+        for i in range(2):
+            response_data = socket_connection.recv(4096)
+            print(response_data)
+            complete_string = complete_string + response_data
+            # content_lines = response_data.decode().split('\r\n')
+            # print(complete_string)
+
+        # print(complete_string)
+        
+        content_lines = complete_string.decode().split('\r\n')
+        version_dict = json.loads(content_lines[10])
+        
+        timestamp = version_dict['read']
+        
+        # Computing the memory use percentage
+
+        used_memory = version_dict['memory_stats']['usage'] - version_dict['memory_stats']['stats']['cache']
+        available_memory = version_dict['memory_stats']['limit']
+        mem_percentage = (used_memory / available_memory) * 100
+
+        # Computing the CPU use percentage
+
+        cpu_delta = version_dict["cpu_stats"]["cpu_usage"]["total_usage"] - version_dict["precpu_stats"]["cpu_usage"]["total_usage"]
+        system_cpu_delta = version_dict["cpu_stats"]["system_cpu_usage"] - version_dict["precpu_stats"]["system_cpu_usage"]
+        number_cpus = version_dict["cpu_stats"]["online_cpus"]
+        cpu_percentage = (cpu_delta / system_cpu_delta) * number_cpus * 100
+
+        # PID Stats 
+
+        num_pid = version_dict["pids_stats"]["current"]
+        csv_writer.writerow([timestamp, mem_percentage, cpu_percentage, num_pid])
+        time.sleep(1)
         
 
 if __name__ == "__main__":
-    container_name = 'echoservice-ttd'
+    container_name = 'mlinfer-nb'
+    file_name = 'mlinfer-nb_new_run.csv'
+    container_stats = DockerStats(container_name=container_name, file_path=file_name)
 
-    container_stats = DockerStats(container_name)
-
-    container_stats.collect_stats()
+    collect_stats(container_stats)
